4b156754cb498d5838dec409287a28a30da9ceb5
anyuz <zhengannyu@gmail.com>
2025-06-01 19:27:28 +0800

    關於我們

---
+++ app/src/main/java/com/example/myapplication_b0253281/AboutWeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication_b0253281
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AboutWeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aboutwe)
+    }
+}
